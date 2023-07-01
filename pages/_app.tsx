7a411b381df3718dcb98fac1715fa927e6d6b29f
AbelPark <parkhm0829@gmail.com>
2023-07-01 14:05:22 +0900

    g-tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/css/tailwind.css'
 import '@/css/prism.css'
+import '@/css/tailwind.css'
 import 'katex/dist/katex.css'
 // import '@/css/docsearch.css' // Uncomment if using algolia docsearch
 // import '@docsearch/css' // Uncomment if using algolia docsearch
@@ -8,23 +8,39 @@ import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import LayoutWrapper from '@/components/LayoutWrapper'
 import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from 'pliny/analytics'
 import { SearchProvider } from 'pliny/search'
-import LayoutWrapper from '@/components/LayoutWrapper'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
-      <LayoutWrapper>
-        <SearchProvider searchConfig={siteMetadata.search}>
-          <Component {...pageProps} />
-        </SearchProvider>
-      </LayoutWrapper>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        <Analytics analyticsConfig={siteMetadata.analytics} />
+        <LayoutWrapper>
+          <SearchProvider searchConfig={siteMetadata.search}>
+            <Component {...pageProps} />
+          </SearchProvider>
+        </LayoutWrapper>
+      </ThemeProvider>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-3F5LBVP7H0`} />
+      <Script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-3F5LBVP7H0');
+              `,
+        }}
+      />
+    </>
   )
 }
